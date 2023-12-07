ed6bd1a77782e3f867b7074fb806387ef130252b
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-12-07 15:44:37 +0530

    Add project files.

---
+++ DokerMvcTesting/Data/DokerMvcTestingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DokerMvcTesting.Models;
+
+namespace DokerMvcTesting.Data
+{
+    public class DokerMvcTestingContext : DbContext
+    {
+        public DokerMvcTestingContext (DbContextOptions<DokerMvcTestingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DokerMvcTesting.Models.Person> Person { get; set; } = default!;
+    }
+}
---
+++ DokerMvcTesting/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace DokerMvcTesting.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
